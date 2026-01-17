7c9a6897ad67b7f0b14a2e6df6048715c56f3dd0
alpheay <180893966+nik-nandi@users.noreply.github.com>
2026-01-17 15:54:02 -0500

    Revert "feat(sdk): implement foundational SDK enhancements"
    This reverts commit e2e16df52841652838975754df3bcc2bf75d6306.

--- packages/playproof-sdk/src/transport/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Transport Module Exports
- * @packageDocumentation
- */
-
-export { HashChain, AsyncHashChain, GENESIS_HASH } from './HashChain';
-export { HttpTransport, type HttpTransportConfig } from './HttpTransport';
