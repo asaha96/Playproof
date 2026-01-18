b5af361ea61d51aa1c20179331a52fcc57ee5be8
alpheay <180893966+nik-nandi@users.noreply.github.com>
2026-01-18 09:33:29 -0500

    Fixing CORS Headers :: Final

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -24,6 +24,20 @@ const nextConfig: NextConfig = {
     // Enable type checking
     ignoreBuildErrors: false,
   },
+  async headers() {
+    return [
+      {
+        // match the api route
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
